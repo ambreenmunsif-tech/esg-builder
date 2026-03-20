1f46826b4cc63500675602503214e67b16e9ae6c
Ali Reza Baqeri <alirezabaqeri972@gmail.com>
2026-03-20 10:12:18 +0100

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-4xl">Hello, World!</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus id vero, tenetur blanditiis minima dicta harum atque? Vitae, voluptates. Natus!
-      </p>
     </div>
   );
 }
