5d7fdc455063ad1a69d114b4c1f636aa93ae24e5
Ali Reza Baqeri <alirezabaqeri972@gmail.com>
2026-03-20 10:51:46 +0100

    shadcn check

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-4xl">Hello, World!</h1>
+      <Button variant="outline">Button</Button>
     </div>
   );
 }
