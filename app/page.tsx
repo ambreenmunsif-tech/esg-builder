4fdc64006c8e58af703aa94f49b54249ac23abdd
lunashaftar-ship-it <luna.shaftar@gmail.com>
2026-03-20 10:02:55 +0100

    test the connection

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-4xl">Hello, World!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus id vero, tenetur blanditiis minima dicta harum atque? Vitae, voluptates. Natus!
+      </p>
     </div>
   );
 }
